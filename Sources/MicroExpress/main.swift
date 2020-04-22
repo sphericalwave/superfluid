d60af86111c65c904f36d35222ee7accfaf5753b
aaronium112 <aaron@sphericalwave.com>
2020-04-22 16:16:19 -0300

    fixed encapsulation violation on router.

--- Sources/MicroExpress/main.swift
+++ Sources/MicroExpress/main.swift
@@ -8,7 +8,40 @@
 
 import NIO
 
+//let todo: (IncomingMessage, ServerResponse, @escaping Next) -> Void = { request, response, next in
+//    response.render("Todolist", [ "title": "DoIt!", "todos": todos ], files: files)
+//}
+
 let router = Router()
+
+//FIXME: what is CORS? // Reusable middleware up here
+router.use(middleware: querystring, cors(allowOrigin: "*"))
+
+// Logging
+router.use { req, res, next in
+    print("\(req.header.method):", req.header.uri)
+    next() // continue processing
+}
+
+router.get(path: "/todos") { _, res, _ in
+    res.render("Todolist", [ "title": "DoIt!", "todos": todos ], files: files)
+}
+
+router.get(path: "/todomvc") { _, res, _ in
+    // send JSON to the browser
+    res.json(model: todos)
+}
+
+router.get(path: "/moo") { req, res, next in
+    res.send(s: "Muhhh")
+}
+
+router.get { req, res, _ in
+    // `param` is provided by querystring
+    let text = req.param(id: "text") ?? "Schwifty"
+    res.send(s: "Hello, \(text) world!")
+}
+
 let eventLoops = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
 let threadPool = BlockingIOThreadPool(numberOfThreads: 4)
 let fileIO = NonBlockingFileIO(threadPool: threadPool)
@@ -20,38 +53,6 @@ let app = WebApp(host: "localhost", port: 8080, router: router, eventLoops: even
 //  guard let data = data else { return print("Failed:", err) }
 //  print("Read passwd:", data)
 //}
-
-//FIXME: what is CORS? // Reusable middleware up here
-app.router.use(middleware: querystring, cors(allowOrigin: "*"))
-
-// Logging
-app.router.use { req, res, next in
-    print("\(req.header.method):", req.header.uri)
-    next() // continue processing
-}
-
-//FIXME: Router Encapsulation Violations
-app.router.get(path: "/todos") { _, res, _ in
-    res.render("Todolist", [ "title": "DoIt!", "todos": todos ], files: files)
-}
-
-//FIXME: Router Encapsulation Violations
-app.router.get(path: "/todomvc") { _, res, _ in
-    // send JSON to the browser
-    res.json(model: todos)
-}
-
-//FIXME: Router Encapsulation Violations
-app.router.get(path: "/moo") { req, res, next in
-    res.send(s: "Muhhh")
-}
-
-//FIXME: Router Encapsulation Violations
-app.router.get { req, res, _ in
-    // `param` is provided by querystring
-    let text = req.param(id: "text") ?? "Schwifty"
-    res.send(s: "Hello, \(text) world!")
-}
 
 app.listen()
 
